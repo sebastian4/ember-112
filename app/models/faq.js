e03b39aa7008f3575ac41e8eadf685ec402de5f8
Sebastian Lopez <sebastian.lopez@rsa.com>
2015-05-13 18:38:31 -0400

    initial exploring

--- app/models/faq.js
+++ app/models/faq.js
@@ -2,5 +2,8 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
 	question: DS.attr('string'),
- 	answer: DS.attr('string')
+ 	answer: DS.attr('string'),
+ 	minianswer: function() {
+	    return this.get('answer').substring(0, 4)+" ...";
+	}.property('answer')
 });
