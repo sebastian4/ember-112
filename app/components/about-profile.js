cfd814eb585d038c73bda5eaefc4d812560868f1
sebastian <sebastianmac@Sebastians-MBP.home>
2015-05-17 01:42:06 -0400

    added about-profile component

---
+++ app/components/about-profile.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  overDiv: false,
+
+  actions: {
+    
+     over: function() {
+            this.toggleProperty('overDiv');
+     },
+     out: function() {
+            this.toggleProperty('overDiv');
+     }
+
+  }
+
+});
