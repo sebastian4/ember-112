e5ae732bc004b93396f77110a7f8d4b0cdbeff06
sebastian <sebastianmac@Sebastians-MBP.home>
2015-05-17 01:06:44 -0400

    added gravatar image component

---
+++ app/components/gravatar-image.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  size: 120,
+  email: '',
+
+  gravatarUrl: function() {
+    var email = this.get('email'),
+        size = this.get('size');
+
+    return 'http://www.gravatar.com/avatar/' + email + '?s=' + size;
+  }.property('email', 'size')
+
+});
