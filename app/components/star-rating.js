09fba8c35678e7641570c5c594732e33b69f1d2f
sebastian <sebastianmac@Sebastians-MBP.home>
2015-05-17 02:38:37 -0400

    added more star rating

--- app/components/star-rating.js
+++ app/components/star-rating.js
@@ -2,6 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  tagName: 'div'
+  maxStars: 0,
+  starRating: 0,
+  stars: [],
+  actions: {
+      click: function(star){
+          this.set('starRating', star.index);
+          this.sendAction('action', star.index);
+      }
+  },
+  setRating: function() {
+      var stars = [];
+      var starRating = this.get('starRating');
+      for(var i = 0; i < this.get('maxStars'); i++){
+          stars.pushObject(Ember.Object.create({empty:i >= starRating, index:i+1}));
+      }
+      this.set('stars', stars);
+  }.observes('starRating').on('didInsertElement')
 
 });
